1fb177fe9f9a8d5d9aa5ad712e7f8fd15ebb2d6d
SowbarnikaArumugam <sowmipavi02@gmail.com>
2023-10-21 20:38:08 +0530

    completed to do list

--- javascript.js
+++ javascript.js
@@ -1,39 +1,37 @@
-let button = document.getElementById('add')
-let todoList = document.getElementById('todoList')
-let input = document.getElementById('input');
-//local storage,cookies
-let todos = [];
-window.onload = ()=>{
-    todos = JSON.parse(localStorage.getItem('todos')) || []
-    todos.forEach(todo=>addtodo(todo))
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Enter your Task");
+    }
+    else{
+        let li = document.createElement("li");
+        li.textContent = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
 }
 
-button.addEventListener('click',()=>{
-    todos.push(input.value)
-    localStorage.setItem('todos',JSON.stringify(todos))
-    addtodo(input.value)
-    input.value=''
-})
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
 
-function addtodo(todo){
-    let para = document.createElement('p');
-    para.innerText = todo;
-    todoList.appendChild(para)
-    
-    para.addEventListener('click',()=>{
-        para.style.textDecoration = 'line-through'
-        remove(todo)
-    })
-    para.addEventListener('dblclick',()=>{
-        todoList.removeChild(para)
-        remove(todo)
-    })
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
 }
-
-function remove(todo){
-    let index = todos.indexOf(todo)
-    if (index > -1) {
-        todos.splice(index, 1);
-      }
-    localStorage.setItem('todos',JSON.stringify(todos))
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
 }
+showData();
